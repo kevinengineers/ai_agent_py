02966ec2565d04887e00400ad70622a8550fab8c
kevinengineers <kevinengineers@proton.me>
2025-06-09 09:11:10 -0700

    deleted ./main.sh, updated main.py to use user input instead of a hardcoded message

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 def main():
-    import os
+    import os, sys
     from dotenv import load_dotenv
     from google import genai
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    prompt = sys.argv[1]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt
     )
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
