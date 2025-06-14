f9135f914f83a67cf78571858c1ee4bef38c7806
kevinengineers <kevinengineers@proton.me>
2025-06-13 22:25:38 -0700

    added config  to response object and printed text for now from a hardcoded var.

--- main.py
+++ main.py
@@ -15,21 +15,25 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = sys.argv[1]
-
+    # system prompt
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
     # setup prompt/repsonse objects
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
 
     if len(argv) > 2:
         print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-
+        # print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
+        # print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+    
+    print(response.text)
 
 if __name__ == "__main__":
     main()
