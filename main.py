6a22877c735af3d15d9e6acf6eb51ec703a46e17
kevinengineers <kevinengineers@proton.me>
2025-06-15 16:50:24 -0700

    cleaned up unneeded lines

--- main.py
+++ main.py
@@ -8,32 +8,61 @@ def main():
     from dotenv import load_dotenv
     from google import genai
     from google.genai import types
-
     load_dotenv()
-
+    
+    
     # initial setup
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = sys.argv[1]
     # system prompt
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    system_prompt = (
+        "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    )
     # setup prompt/repsonse objects
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description=(
+            "Lists files in the specified directory along with their sizes, "
+            "constrained to the working directory."
+        ),
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+
     response = client.models.generate_content(
+
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
     )
 
     if len(argv) > 2:
         print(f"User prompt: {user_prompt}")
         # print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
         # print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-    
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
